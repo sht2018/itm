4f74a9b10d94361b2f8bd67ecd437933b9b28409
Icarus Radio <songyj@shanghaitech.edu.cn>
2018-11-13 15:32:50 +0800

    My shabby_house.

---
+++ students/syj/shabby_house.py
@@ -0,0 +1,28 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import secrets
+mc = minecraft.Minecraft.create()
+blocks = [i for i in range(1, 10)]
+
+def shabby_house(x, y, z):
+    for i in range(-5, 6):
+        for j in range(-5, 6):
+            mc.setBlock(x - 5, y + i, z + j, secrets.choice(blocks))
+    for i in range(-5, 6):
+        for j in range(-5, 6):
+            mc.setBlock(x + 5, y + i, z + j, secrets.choice(blocks))
+    for i in range(-5, 6):
+        for j in range(-5, 6):
+            mc.setBlock(x + i, y - 5, z + j, secrets.choice(blocks))
+    for i in range(-5, 6):
+        for j in range(-5, 6):
+            mc.setBlock(x + i, y + 5, z + j, secrets.choice(blocks))
+    for i in range(-5, 6):
+        for j in range(-5, 6):
+            mc.setBlock(x + i, y + j, z - 5, secrets.choice(blocks))
+    for i in range(-5, 6):
+        for j in range(-5, 6):
+            mc.setBlock(x + i, y + j, z + 5, secrets.choice(blocks))
+
+pos = mc.player.getTilePos()
+shabby_house(pos.x, pos.y, pos.z)
