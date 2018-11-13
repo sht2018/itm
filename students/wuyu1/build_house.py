fb41df80b2a72dd4f0f3e46c22d4b38d199d76d5
wuyu1 <wuyu1@shanghaitech.edu.cn>
2018-11-13 17:25:56 +0800

    only editted the name of the program

---
+++ students/wuyu1/build_house.py
@@ -0,0 +1,23 @@
+from mcpi.minecraft import Minecraft
+import time
+
+mc=Minecraft.create()
+# mc=Minecraft.create("192.168.2.207",4711)
+
+while True:
+    hits=mc.events.pollBlockHits() 
+    for hit in hits:
+        mc.postToChat("Hit:"+"x"+str(hit.pos.x)+"y"+str(hit.pos.y)+"z"+str(hit.pos.z))
+        for x in range(10):    
+            for y in range(10):
+                mc.setBlock(hit.pos.x + x, hit.pos.y + y, hit.pos.z, 1)
+            for z in range(10):
+                mc.setBlock(hit.pos.x, hit.pos.y + x, hit.pos.z + z, 1)
+            for a in range(10):
+                mc.setBlock(hit.pos.x + 9, hit.pos.y + x, hit.pos.z + a, 1)
+            for b in range(10):
+                mc.setBlock(hit.pos.x + b, hit.pos.y + x, hit.pos.z + 9 , 1)
+
+
+        
+     
