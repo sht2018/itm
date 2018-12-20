dfe342391a83d4e0e727919145e5be722ca0c045
zhangqx0628 <44995428+zhangqx0628@users.noreply.github.com>
2018-12-20 18:08:09 +0800

    Add files via upload

---
+++ students/zhangqixing/song.ino
@@ -0,0 +1,41 @@
+#define BuzzerPin6 6
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available() > 0) {
+    switch (String(Serial.readString()).toInt()) {
+     case 1:
+      tone(BuzzerPin6,523);
+      break;
+     case 2:
+      tone(BuzzerPin6,587);
+      break;
+     case 3:
+      tone(BuzzerPin6,659);
+      break;
+     case 4:
+      tone(BuzzerPin6,698);
+      break;
+     case 5:
+      tone(BuzzerPin6,784);
+      break;
+     case 6:
+      tone(BuzzerPin6,880);
+      break;
+     case 7:
+      tone(BuzzerPin6,988);
+      break;
+     case 8:
+      tone(BuzzerPin6,1046);
+      break;
+    }
+    delay(500);
+    noTone(BuzzerPin6);
+
+  }
+
+}
